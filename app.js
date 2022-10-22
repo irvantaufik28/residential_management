d36c4d6ea1a1b1e0fbd97e3e4915b43dfc9807b7
irvan taufik <irvantaufik28@gmail.com>
2022-10-22 13:24:45 +0700

    add otp veryfy

--- app.js
+++ app.js
@@ -8,15 +8,20 @@ const bcrypt = require("bcrypt")
 const generateAccessToken = require("./helper/jwt")
 const serverError = require("./middleware/serverError")
 const _ = require("lodash")
+const func = require('./libs/function')
+const email_message = require('./internal/constant')
 
 
 // import repository
 const UserRepository = require('./repository/user')
 const AuthRepository = require('./repository/auth')
-
+const OtpRepository = require('./repository/otp')
+const EmailRepository = require('./repository/email')
 
 // import Usecase
 const AuthUseCase = require('./usecase/auth')
+const UserUseCase = require('./usecase/user')
+const OtpUsecase = require('./usecase/otp')
 
 
 // intit router
@@ -30,10 +35,23 @@ const authUC = new AuthUseCase(
   generateAccessToken,
   _
 )
+const userUC = new UserUseCase(
+  new UserRepository(),
+  new OtpRepository(),
+  bcrypt,
+);
 
+
+const otpUC = new OtpUseCase(
+  new OtpRepository(),
+  new EmailRepository(),
+  email_message
+);
 
 app.use((req, res, next) => {
   req.authUC = authUC;
+  req.otpUC = otpUC;
+  req.userUC = userUC;
   next();
 });
 
