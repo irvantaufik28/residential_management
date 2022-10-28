62e2472451bfa64de1a958191b1adcbc1771502c
irvan taufik <irvantaufik28@gmail.com>
2022-10-28 10:44:18 +0700

    pull

--- usecase/identityCard.js
+++ usecase/identityCard.js
@@ -2,7 +2,7 @@ class indentityCard {
   constructor(IdentityCardRepository, UserRepository, card_type, _) {
     this.IdentityCardRepository = IdentityCardRepository;
     this.UserRepository = UserRepository;
-    this.card_type = card_type
+    this.card_type = card_type;
     this._ = _;
   }
 
@@ -14,27 +14,28 @@ class indentityCard {
       data: null,
     };
 
-    const identityCard = await this.IdentityCardRepository.getIdentityCardById(id);
+    const identityCard = await this.IdentityCardRepository.getIdentityCardById(
+      id
+    );
     if (identityCard === null) {
-      result.reason = "identity card not found"
-      return result
+      result.reason = "identity card not found";
+      return result;
     }
-    const user = await this.UserRepository.getUserById(identityCard.userId)
+    const user = await this.UserRepository.getUserById(identityCard.userId);
     if (user === null) {
-      result.reason = "user not found, shometing went wrong"
-      return result
+      result.reason = "user not found, shometing went wrong";
+      return result;
     }
     const detialData = {
       identityCard: identityCard,
-      user: user
-    }
+      user: user,
+    };
 
     result.isSuccess = true;
     result.status = 200;
     result.data = detialData;
     return result;
   }
-
 
   async getIdentityCardByUserId(userId) {
     let result = {
@@ -44,15 +45,14 @@ class indentityCard {
       data: [],
     };
 
-
-    const indentityCard = await this.IdentityCardRepository.getIdentityCardByUserId(userId);
+    const indentityCard =
+      await this.IdentityCardRepository.getIdentityCardByUserId(userId);
 
     result.isSuccess = true;
     result.status = 200;
     result.data = indentityCard;
     return result;
   }
-
 
   async getIdentityCardByCardType(card_type) {
     let result = {
@@ -62,8 +62,8 @@ class indentityCard {
       data: [],
     };
 
-
-    const indentityCard = await this.IdentityCardRepository.getIdentityCardByCardType(card_type);
+    const indentityCard =
+      await this.IdentityCardRepository.getIdentityCardByCardType(card_type);
     if (indentityCard === null) {
       result.reason = "indentityCard not found";
     }
@@ -80,19 +80,24 @@ class indentityCard {
       reason: null,
       data: null,
     };
-    const userCard = await this.IdentityCardRepository.getIdentityCardByUserId(data.userId)
+    const userCard = await this.IdentityCardRepository.getIdentityCardByUserId(
+      data.userId
+    );
     for (let i = 0; i < userCard.length; i++) {
       if (userCard[i].card_type === data.card_type) {
-        result.reason = `failed, cannot add ${data.card_type} more than one`
-        return result
+        result.reason = `failed, cannot add ${data.card_type} more than one`;
+        return result;
       }
     }
-    const existCard = await this.IdentityCardRepository.getIdentityCardBySerialNo(data.serialNo)
+    const existCard =
+      await this.IdentityCardRepository.getIdentityCardBySerialNo(
+        data.serialNo
+      );
     if (existCard !== null) {
-      result.reason = `failed, ${data.card_type} already registered`
-      return result
+      result.reason = `failed, ${data.card_type} already registered`;
+      return result;
     }
-   const indentityCard = await this.IdentityCardRepository.createCard(data);
+    const indentityCard = await this.IdentityCardRepository.createCard(data);
     result.isSuccess = true;
     result.status = 201;
     result.data = indentityCard;
@@ -107,18 +112,27 @@ class indentityCard {
       data: null,
     };
 
-    const indentityCard = await this.IdentityCardRepository.getIdentityCardById(id);
+    const indentityCard = await this.IdentityCardRepository.getIdentityCardById(
+      id
+    );
     if (indentityCard === null) {
       result.reason = "indentityCard not found";
       return result;
     }
-    const existCard = await this.IdentityCardRepository.getIdentityCardBySerialNo(indentityCardData.serialNo)
+    const existCard =
+      await this.IdentityCardRepository.getIdentityCardBySerialNo(
+        indentityCardData.serialNo
+      );
     if (existCard !== null) {
-      result.reason = "failed, card already registered"
-      result.status = 400
-      return result
+      result.reason = "failed, card already registered";
+      result.status = 400;
+      return result;
     }
-    const newindentityCard = await this.IdentityCardRepository.updateIdentityCard(indentityCardData, id);
+    const newindentityCard =
+      await this.IdentityCardRepository.updateIdentityCard(
+        indentityCardData,
+        id
+      );
     result.isSuccess = true;
     result.status = 204;
     result.data = newindentityCard;
@@ -132,11 +146,17 @@ class indentityCard {
       reason: null,
       data: null,
     };
-    let indentityCard = await this.IdentityCardRepository.getIdentityCardById(id);
+    let indentityCard = await this.IdentityCardRepository.getIdentityCardById(
+      id
+    );
     if (indentityCard === null) {
       result.reason = "indentityCard not found";
       return result;
     }
+    const setUserToUnrigestered = {
+      isRegistered: false,
+    };
+    await this.UserRepository.updateUser(indentityCard.userId, setUserToUnrigestered);
     await this.IdentityCardRepository.deleteIdentityCard(id);
     result.isSuccess = true;
     result.status = 204;
